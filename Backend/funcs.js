89017d6e915160f1e1aac56f97fc137b33b24ca1
Arihant Bapna <greatbapna@gmail.com>
2021-11-06 22:10:02 -0400

    QA

--- Backend/funcs.js
+++ Backend/funcs.js
@@ -2,27 +2,31 @@ const userModel = require('./models/users');
 const mongoose = require("mongoose");
 
 module.exports = {
-    generateTextId: function(length){
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    generateTextId: function (length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
+        for (var i = 0; i < length; i++) {
             result += characters.charAt(Math.floor(Math.random() *
                 charactersLength));
         }
         return result;
     },
     formValidation: async function (user) {
-        errs = []
+        let errs = []
+
+        if (!user.username) errs.push('Add a username');
+        if (!user.firstname || !user.lastname) errs.push('Have a name')
+
         let repeat = await userModel.findOne({email: user.email});
-        if (repeat){
-            errs = errs.concat('Email already exists')
+        if (repeat) {
+            errs.push('Email already exists')
         }
         let userRepeat = await userModel.findOne({username: user.username});
-        if (userRepeat){
-            errs = errs.concat('Username already exists');
+        if (userRepeat) {
+            errs.push('Username already exists');
         }
-        if (user.password.length === 0) errs = errs.concat('Password empty');
+        if (user.password.length === 0) errs.push('Password empty');
         return errs;
     }
 }
